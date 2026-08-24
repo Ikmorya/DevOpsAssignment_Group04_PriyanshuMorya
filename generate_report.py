c4d55d955d9e6f40bd39567048c88cf2bfafa19b
Priyanshu Dangi <priyanshudangi02pcm@gmail.com>
2026-08-24 14:15:00 +0530

    feat: add generate_report.py - PDF report generation script

---
+++ generate_report.py
@@ -0,0 +1,43 @@
+import subprocess
+from collections import defaultdict
+import datetime
+import io
+import os
+import sys
+import html
+import matplotlib.pyplot as plt
+
+
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image, KeepTogether
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+
+# -------------------------------------------------------------
+# CONFIGURATION: Institution & Department Details
+# -------------------------------------------------------------
+COLLEGE_NAME = "Swami Keshvanand Institute of Technology, Management & Gramothan, Jaipur"
+DEPARTMENT_NAME = "Department of Computer Science & Engineering"
+# -------------------------------------------------------------
+
+
+def get_repo_info():
+    """Extracts the repository name and current branch."""
+    repo_name = "Project-Repository"
+    branch_name = "main"
+
+
+    try:
+        root_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], encoding='utf-8').strip()
+        repo_name = os.path.basename(root_path)
+    except Exception:
+        try:
+            remote_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'], encoding='utf-8').strip()
+            repo_name = remote_url.rstrip('/').split('/')[-1].replace('.git', '')
+        except Exception:
+            repo_name = os.path.basename(os.getcwd())
+
+
+    try:
+        branch_name = subprocess.check_output(['git', '
